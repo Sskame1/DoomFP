094f858553915c49cbae58e60dd43f872250d56a
Anatoliy <119749324+Sskame1@users.noreply.github.com>
2024-11-26 19:26:40 +0900

    Added PlayerMove

--- Assets/script/PlayerMove.cs
+++ Assets/script/PlayerMove.cs
@@ -1,18 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float moveSpeed = 5f;
+    public Transform cameraTransform;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+
+        Vector3 forward = cameraTransform.forward;
+        Vector3 right = cameraTransform.right;
+
+        forward.y = 0;
+        right.y = 0;
+        forward.Normalize();
+        right.Normalize();
+
+        Vector3 direction = (forward * ver + right * hor).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+
+            transform.position += direction * moveSpeed * Time.deltaTime;
+        }
     }
 }
